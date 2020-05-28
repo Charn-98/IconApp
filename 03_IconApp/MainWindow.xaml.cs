258180c7f34da7c0232d20c3cbebfab4c3fc3dd4
charn <charnvdw64@gmail.com>
2020-05-28 15:26:36 +0200

    Pushing last project

--- 03_IconApp/MainWindow.xaml.cs
+++ 03_IconApp/MainWindow.xaml.cs
@@ -20,9 +20,69 @@ namespace _03_IconApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        public Dictionary<int, string> oNames = new Dictionary<int, string>();
+
         public MainWindow()
         {
             InitializeComponent();
+            dtResults.ItemsSource = oNames;
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            int iCount = oNames.Count + 1;
+
+            if (!string.IsNullOrEmpty(txtName.Text))
+            {
+                oNames.Add(iCount, txtName.Text);
+                dtResults.ItemsSource = null;
+                dtResults.ItemsSource = oNames;
+                txtName.Text = string.Empty;
+            }
+            else
+            {
+                MessageBox.Show("Field was left empty. Please provide valid name.", "Invalid Input", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+        private void BtnGet_Click(object sender, RoutedEventArgs e)
+        {
+            string sName = string.Empty;
+            int iItemNumber = 0;
+
+            try
+            {
+                if (int.TryParse(txtNumber.Text, out iItemNumber))
+                {
+                    if (iItemNumber <= oNames.Count)
+                    {
+                        if (oNames.Count(x => x.Key == iItemNumber) != 0)
+                        {
+                            sName = oNames.Where(x => x.Key == iItemNumber)
+                                          .Select(x => x.Value)
+                                          .FirstOrDefault();
+
+                            lblResult.Content = $"{iItemNumber}: {sName}";
+                        }
+                        else
+                        {
+                            MessageBox.Show($"Corresponding name could not be found for {iItemNumber}", "Invalid Input", MessageBoxButton.OK, MessageBoxImage.Warning);
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show($"Sorry, there is nothing do display for number {iItemNumber}. Please enter numbers 1 - {oNames.Count}", "Invalid Input", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show($"Number {txtNumber.Text} is invalid. Name could not be retrieved", "Invalid Input", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+            }
+            catch (Exception)
+            {
+                    MessageBox.Show($"There has been a problem. Please try again", "System Error", MessageBoxButton.OK, MessageBoxImage.Hand);
+            }
         }
     }
 }
